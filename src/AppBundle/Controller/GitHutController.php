4c38b969392253aa991523e201b29eee30f7c2ad
Dalius Burokas <daliusbu@gmail.com>
2018-02-08 17:11:38 +0200

    Pries Services.yml

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Service\GithubApi;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,34 +20,19 @@ class GitHutController extends Controller
     /**
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/", name ="githut")
+     * @Route("/{username}", name ="githut", defaults={"username":"daliusbu"})
      */
-    public function githutAction(Request $request)
+    public function githutAction(Request $request, $username)
     {
 
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.github.com/users/daliusbu');
-        dump($response);
+       $data = (new GithubApi())->getProfile($username);
 
 
         return $this->render('githut/index.html.twig',
             [
-                'avatar_url' => 'https://avatars0.githubusercontent.com/u/25664565?v=4',
-                'name' => 'Dalius',
-                'login' => 'daliusbu',
-                'details' => [
-                    'company' => "Kompanija",
-                    'location' => 'Kaunas',
-                    'joined_on' => 'Jonied on 2017.02.02',
-                ],
-                'blog' => 'Mano blogas',
-                'social_data' => [
-                    "Public Repos" => 12,
-                    "Followers" => 10,
-                    "Following" => 30,
-                ],
-                'repo_count'=>5,
-                'most_stars'=>4,
+
+                'repo_count'    => 10, //     $data['repo_count'],
+                'most_stars'    => 20,  //   $data['most_stars'],
                 'repos' =>[
                     [
                         'name'=> 'debugger',
--- src/AppBundle/Service/GithubApi.php
+++ src/AppBundle/Service/GithubApi.php
@@ -6,12 +6,34 @@
  * Time: 16.17
  */
 
-namespace \AppBundle\Service;
+namespace AppBundle\Service;
+
+use GuzzleHttp;
 
 class GithubApi
 {
 
-    public function getProfile(){
+    public function getProfile($username)
+    {
+        $client = new GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.github.com/users/' . $username);
+//        dump($response->getBody()->getContents());
+        $data = json_decode($response->getBody()->getContents(), true);
 
+        return ['avatar_url' => $data['avatar_url'],
+            'name' => $data['name'],
+            'login' => $data['login'],
+            'details' => [
+                'company' => $data['company'],
+                'location' => $data['location'],
+                'joined_on' => 'Joined on ' . (new \DateTime($data['created_at']))->format('Y-m-d'),
+            ],
+            'blog' => 'Mano blogas',
+            'social_data' => [
+                "Public Repos" => $data['public_repos'],
+                "Followers" => $data['followers'],
+                "Following" => $data['following'],
+            ],
+        ];
     }
 }
